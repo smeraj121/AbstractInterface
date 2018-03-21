3a2041a83629e66e982a1bc7ffd558545ecbe3b9
Saba <smeraj121@gmail.com>
2018-03-21 14:49:38 +0530

    Add Repository

---
+++ interfaces_abstract/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace interfaces_abstract
+{
+    abstract class Polygon
+    {
+        public abstract int Area(int l);
+        protected virtual void Sides() { }
+    }
+
+    interface IShape
+    {
+        int Perimeter(int l, int b);
+    }
+
+    class Square : Polygon, IShape
+    {
+        public override int Area(int l)
+        {
+            return l * l;
+        }
+
+        protected override void Sides()
+        {
+            Console.WriteLine("4");
+        }
+
+        public int Perimeter(int l, int b)
+        {
+            return 2 * l + 2 * b;
+        }
+    }
+
+    class Circle : IShape
+    {
+        public int Perimeter(int l, int b = 0)
+        {
+            return 2 * 3 * l;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polygon poly = new Square();
+            Square sq = new Square();
+            IShape sh = new Square();
+            int peri = sh.Perimeter(2, 5);
+            Circle cir = new Circle();
+            cir.Perimeter(8);
+        }
+    }
+}
